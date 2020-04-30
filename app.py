e64b298538e853ba8dd68bc47916e8c3bddc13f1
Samantha Rand <samantharand@Samanthas-MacBook-Pro-2.local>
2020-04-30 10:54:12 -0500

    users route connects in postman

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 import models
 
+from resources.users import users
+
 DEBUG=True
 PORT=8000
 
 app = Flask(__name__)
+
+app.register_blueprint(users, url_prefix="/users")
 
 @app.route('/', methods=['GET'])
 def hello_world():
--- resources/users.py
+++ resources/users.py
@@ -1,1 +1,10 @@
 import models
+
+from flask import Blueprint, request
+
+users = Blueprint('users', 'users')
+
+@users.route('/', methods=['GET'])
+def users_test_route():
+	print('users_test_route')
+	return 'users_test_route'
